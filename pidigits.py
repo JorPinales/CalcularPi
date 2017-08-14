15d2739e580439e6f9a0613b732d13e13cb91bec
Jesus Neri <30955414+neriphy@users.noreply.github.com>
2017-08-14 17:40:16 -0500

    Add files via upload

--- pidigits.py
+++ pidigits.py
@@ -3,12 +3,14 @@
 
 from math import *
 import webbrowser
+from datetime import datetime
 
 n = 2
 
 respusta = 1
 
 limite = int(input("Introduzca limite de suma: "))
+instanteInicial = datetime.now()
 
 while limite >= n:
 	respusta = 1/(n**2) + respusta
@@ -18,6 +20,12 @@ while limite >= n:
 
 
 respusta = sqrt(6*(respusta))
+
+instanteFinal = datetime.now()
+tiempo = instanteFinal - instanteInicial
+segundos = tiempo.seconds
+microsegundos = tiempo.microseconds
+print(microsegundos,"microsegundos tardo en calcular o,", segundos,"segundos")
 
 
 print("\nEl programa calculo que pi es aproximadamente:",respusta)
